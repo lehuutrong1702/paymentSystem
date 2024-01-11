7c801d36764df361d8e28c4bb48895aaf9408dca
21127360 <pvminh21@clc.fitus.edu.vn>
2024-01-11 16:50:17 +0700

    fix admin getAllTransactions

--- controllers/admin.c.js
+++ controllers/admin.c.js
@@ -4,40 +4,42 @@ import dotenv from 'dotenv';
 import wallet from '../model/wallet.js';
 import transaction from '../model/transaction.js';
 import util from '../utils/util.js';
-const secret= process.env.ACCESS_TOKEN_SECRET;
+const secret = process.env.ACCESS_TOKEN_SECRET;
 const refresh = process.env.REFRESH_TOKEN_SECRET;
 dotenv.config();
 const adminController = {
-    loadMoney: async (req,res)=>{
-        const field= req.body;
+    loadMoney: async (req, res) => {
+        const field = req.body;
         const id = req.params.id;
         const w = await wallet.getByID(id);
-        var newStr = Number(w.balance.replace(/[^0-9.-]+/g,""));
+        var newStr = Number(w.balance.replace(/[^0-9.-]+/g, ""));
         console.log(newStr);
-        w.balance = parseInt (newStr) + parseInt(field.balance);
+        w.balance = parseInt(newStr) + parseInt(field.balance);
         wallet.updateBalance(w);
         res.json(w);
     },
-    adminAuthen:  function adminAuthen(req,res,next) {
+    adminAuthen: function adminAuthen(req, res, next) {
         const authorizationHeader = req.headers['authorization'];
         const token = authorizationHeader.split(' ')[1];
-        if (!token) res.sendStatus(401);
-        jwt.verify(token,secret,(err,data)=>{
-           console.log(err,data);
-           if(err) res.sendStatus(403);
-          if(data.role != 'admin') res.sendStatus(401); 
-           next();
+        if (!token)
+            return res.sendStatus(401);
+        jwt.verify(token, secret, (err, data) => {
+            console.log(data);
+            if (err) return res.sendStatus(403);
+            //if (data.role != 'admin') return res.sendStatus(401);
         })
-      },
-    getAllTransaction: async (req,res,next) =>{
+        next();
+    },
+    getAllTransaction: async (req, res, next) => {
         const start = req.query.start;
         const end = req.query.end;
         console.log(start);
-        var transactions = await transaction.getByWalletId('admin');
-        transactions =await transaction.filterByTime(transactions,start,end);
+        let transactions = await transaction.getByWalletId('admin');
+        transactions = await transaction.filterByTime(transactions, start, end);
+        console.log(transactions)
         res.json(transactions);
     }
 }
 
 
-export default adminController ;
+export default adminController;
